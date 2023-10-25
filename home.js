beb770822ecf48ff10a1d430ee750ef6dc700cca
IBRA-oub <brahimbourih@gmail.com>
2023-10-25 16:40:08 +0100

    new commit

--- home.js
+++ home.js
@@ -1,6 +1,22 @@
+
+//burger-menu-de-tout-les-pages
+
 const burger=document.querySelector('.burger-menu');
 const showBurger=document.querySelector('.top-area .navbar');
 
 burger.addEventListener('click',()=>{
     showBurger.classList.toggle('showburger');
 })
+
+//-------------------FAQ--------------
+
+
+const question=document.getElementsByClassName("question");
+const anser=document.getElementsByClassName("anser");
+
+for(let i=0 ; i< question.length ; i++){
+    question[i].addEventListener('click', () => {
+        question[i].classList.toggle('showmoins');
+        anser[i].classList.toggle('showanser');
+    })
+}
