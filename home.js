068b827f39d6cd44f0c4505fd6d2faa4b8018f92
IBRA-oub <brahimbourih@gmail.com>
2023-10-25 16:58:42 +0100

    new commit responsive

--- home.js
+++ home.js
@@ -1,6 +1,5 @@
 
-//burger-menu-de-tout-les-pages
-
+//---------burger-menu-de-tout-les-pages-------
 const burger=document.querySelector('.burger-menu');
 const showBurger=document.querySelector('.top-area .navbar');
 
@@ -20,3 +19,22 @@ for(let i=0 ; i< question.length ; i++){
         anser[i].classList.toggle('showanser');
     })
 }
+
+//--------------------how it's made scroll---------------
+
+window.addEventListener("scroll", function() {
+    var elements = document.querySelectorAll(".etapesec");
+  
+    elements.forEach(function(element) {
+      var position = element.getBoundingClientRect();
+  
+      
+      if ((position.top < window.innerHeight/1.5)) {
+        element.classList.add("fade-in");
+      }
+    });
+  });
+
+  //------------------------input-validation----------------
+
+  
